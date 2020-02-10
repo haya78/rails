41d15db2e9124e7b03cc9fa705a634bfd99a743e
EC2 Default User <ec2-user@ip-172-31-44-39.us-east-2.compute.internal>
2020-02-10 06:29:28 +0000

    tech boost

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  #名前の長さに15文字までの制限を追加
+  validates :name, presence: true, length: {maximum: 15}
+  #メールアドレスの正規表現
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  #パスワードの文字数を8~32文字のみ,数字の混合のみ可能
+  validates :password, presence: true, length: { in: 8..32 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i}
+  has_secure_password
+  
+end
